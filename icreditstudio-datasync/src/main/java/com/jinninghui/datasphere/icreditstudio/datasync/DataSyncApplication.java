d054949f1dce97e383506d6ccc8ddc1f48894da0
pengpai <pengpai@qq.com>
2021-08-20 11:40:15 +0800

    配置

--- icreditstudio-datasync/src/main/java/com/jinninghui/datasphere/icreditstudio/datasync/DataSyncApplication.java
+++ icreditstudio-datasync/src/main/java/com/jinninghui/datasphere/icreditstudio/datasync/DataSyncApplication.java
@@ -1,15 +1,19 @@
 package com.jinninghui.datasphere.icreditstudio.datasync;
 
+import com.jinninghui.datasphere.icreditstudio.framework.sequence.configuration.EnableSequenceService;
+import com.jinninghui.datasphere.icreditstudio.framework.validate.EnableValidator;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-@EnableTransactionManagement
+@SpringBootApplication()
+@EnableSequenceService
+@EnableValidator
 @EnableScheduling
-@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.jinninghui.**.mapper"})
+@EnableAspectJAutoProxy(exposeProxy = true)//exposeProxy类内部可以获取到当前类的代理对象
 public class DataSyncApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataSyncApplication.class, args);
