0d881a72aff1717cfbd281722f7b6dc67f119583
lizheng <lizheng@newspiral.net>
2021-09-27 18:50:41 +0800

    feat: 新增字典表页面

---
+++ icreditstudio-frontend/src/views/icredit/configuration/form/data-manage-dictionary.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: lizheng
+ * @Description: 字典表
+ * @Date: 2021-09-27
+ */
+export default [
+  {
+    type: 'text',
+    label: '字典名称',
+    model: '',
+    ruleProp: 'name',
+    isSearch: false
+  }
+]
