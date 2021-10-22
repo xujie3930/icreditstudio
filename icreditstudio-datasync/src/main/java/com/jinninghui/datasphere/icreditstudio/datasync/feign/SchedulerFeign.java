363ef105b9b0267c4366122ba163180af0f4f1ea
lmh <liminghui@newspiral.net>
2021-10-22 14:38:53 +0800

    代码调整

--- icreditstudio-datasync/src/main/java/com/jinninghui/datasphere/icreditstudio/datasync/feign/SchedulerFeign.java
+++ icreditstudio-datasync/src/main/java/com/jinninghui/datasphere/icreditstudio/datasync/feign/SchedulerFeign.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.jinninghui.datasphere.icreditstudio.datasync.feign.request.FeignCreatePlatformProcessDefinitionRequest;
 import com.jinninghui.datasphere.icreditstudio.datasync.feign.result.CreatePlatformTaskResult;
 import com.jinninghui.datasphere.icreditstudio.framework.result.BusinessResult;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
