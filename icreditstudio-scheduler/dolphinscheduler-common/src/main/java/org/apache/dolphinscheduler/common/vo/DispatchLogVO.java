7b0b67987fdd7095110e4e863d63c20e68520bdd
lmh <liminghui@newspiral.net>
2021-11-08 11:16:07 +0800

    --bug=1018982 --user=liminghui 【同步任务调度】草稿箱状态的同步任务不应出现在同步任务调度列表中 --bug=1018983 --user=liminghui 【同步任务调度】查看任务调度日志接口（/dolphinscheduler/dispatch/log/page）返回的调度时间应精确至秒

--- icreditstudio-scheduler/dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/vo/DispatchLogVO.java
+++ icreditstudio-scheduler/dolphinscheduler-common/src/main/java/org/apache/dolphinscheduler/common/vo/DispatchLogVO.java
@@ -14,7 +14,7 @@ public class DispatchLogVO {
     //task任务实例状态
     private Integer taskInstanceState;
     //task任务执行时间
-    private String taskInstanceExecTime;
+    private Date taskInstanceExecTime;
     //task任务执行时长
     private Long taskInstanceExecDuration;
     //task任务执行 同步数据量
