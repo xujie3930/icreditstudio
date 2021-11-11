1d77ce8016ecf03dcb68999e952cf90e0acce4d4
lmh <liminghui@newspiral.net>
2021-11-11 17:22:59 +0800

    --task=1017156 --user=liminghui 【新】【同步任务调度】-历史执行情况新增重跑和终止操作，列表中重跑改为立即执行 --task=1017154 --user=liminghui 【新】【同步任务调度】-历史执行情况分页显示+筛选条件

--- icreditstudio-scheduler/dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/request/LogPageRequest.java
+++ icreditstudio-scheduler/dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/request/LogPageRequest.java
@@ -1,11 +1,19 @@
 package org.apache.dolphinscheduler.api.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jinninghui.datasphere.icreditstudio.framework.result.base.BusinessBasePageForm;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class LogPageRequest extends BusinessBasePageForm {
 
     private String taskId;
+    private Integer taskStatus;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date execTimeStart;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Date execTimeEnd;
 
 }
