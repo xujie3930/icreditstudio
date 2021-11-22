2ede02c760c1a35c89d0c7d0d722dbcc217543d8
lmh <liminghui@newspiral.net>
2021-11-22 18:05:23 +0800

    代码调整

--- icreditstudio-scheduler/dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/DispatchServiceImpl.java
+++ icreditstudio-scheduler/dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/impl/DispatchServiceImpl.java
@@ -189,15 +189,15 @@ public class DispatchServiceImpl implements DispatchService {
     public BusinessResult<Boolean> nowRun(String taskId) {
         String definitionId = dataSyncDispatchTaskFeignClient.getProcessDefinitionIdByTaskId(taskId);
         ProcessInstance processInstance = processInstanceMapper.getLastInstanceByDefinitionId(definitionId);
+        if(null == processInstance){//第一次执行，
+            platformExecutorService.execSyncTask(definitionId);
+            return BusinessResult.success(true);
+        }
         if (processInstance.getState() == ExecutionStatus.RUNNING_EXECUTION || processInstance.getState() == ExecutionStatus.SUBMITTED_SUCCESS ||
                 processInstance.getState() == ExecutionStatus.WAITTING_THREAD) {//该任务正在 【执行中】中，不能执行
             throw new AppException(ResourceCodeBean.ResourceCode.RESOURCE_CODE_60000013.code, ResourceCodeBean.ResourceCode.RESOURCE_CODE_60000013.message);
         }
         dataSyncDispatchTaskFeignClient.updateExecStatusByScheduleId(definitionId);
-        if(null == processInstance){//第一次执行，
-            platformExecutorService.execSyncTask(definitionId);
-            return BusinessResult.success(true);
-        }
         handleProcessInstance(processInstance);
         int result = insertCommand(processInstance.getId(), definitionId, CommandType.REPEAT_RUNNING);
         if(0 == result){
