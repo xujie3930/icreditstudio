aa20e9b4a100b4f997b76c72cdf7fd5bd2ecbb1d
pengpai <1060127741@qq.com>
2021-12-08 18:30:12 +0800

    --task=1018797 --user=彭湃 【数据同步】-周期任务参数动态替换

--- icreditstudio-datasync/src/main/java/com/jinninghui/datasphere/icreditstudio/datasync/service/task/reader/mysql/MysqlReaderConfigParam.java
+++ icreditstudio-datasync/src/main/java/com/jinninghui/datasphere/icreditstudio/datasync/service/task/reader/mysql/MysqlReaderConfigParam.java
@@ -1,26 +1,11 @@
 package com.jinninghui.datasphere.icreditstudio.datasync.service.task.reader.mysql;
 
+import com.jinninghui.datasphere.icreditstudio.datasync.service.task.reader.ReaderConfigParam;
 import lombok.Data;
 
 /**
  * @author Peng
  */
 @Data
-public class MysqlReaderConfigParam {
-    /**
-     * 数据库用户名
-     */
-    private String username;
-    /**
-     * 数据库密码
-     */
-    private String password;
-    /**
-     * jdbcUrl
-     */
-    private String jdbcUrl;
-    /**
-     * 查询语句
-     */
-    private String querySql;
+public class MysqlReaderConfigParam extends ReaderConfigParam {
 }
