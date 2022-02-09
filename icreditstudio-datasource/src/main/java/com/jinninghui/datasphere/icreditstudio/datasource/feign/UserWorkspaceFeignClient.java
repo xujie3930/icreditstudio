46d6b67dcf0656c59e0d47ba278bae9932acbaa7
xujie <xujie-bl@bestpay.com.cn>
2022-02-09 16:25:15 +0800

    Revert "父pom升级nacos版本至2.2.6.RELEASE"
    This reverts commit c82c37a7

--- icreditstudio-datasource/src/main/java/com/jinninghui/datasphere/icreditstudio/datasource/feign/UserWorkspaceFeignClient.java
+++ icreditstudio-datasource/src/main/java/com/jinninghui/datasphere/icreditstudio/datasource/feign/UserWorkspaceFeignClient.java
@@ -1,23 +1,23 @@
-//package com.jinninghui.datasphere.icreditstudio.datasource.feign;
-//
-//import com.jinninghui.datasphere.icreditstudio.framework.result.BusinessResult;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-///**
-// * @author xujie
-// * @description iCreditBanner类
-// * @create 2021-08-19 14:17
-// **/
-//@Component
-//@FeignClient("workspace")
-//public interface UserWorkspaceFeignClient {
-//
-//    @GetMapping("/workspace/user/getWorkspaceByUserId/{id}")
-//    BusinessResult<List<Map<String, String>>> getWorkspaceListByUserId(@PathVariable(value = "id", required = false) String id);
-//}
+package com.jinninghui.datasphere.icreditstudio.datasource.feign;
+
+import com.jinninghui.datasphere.icreditstudio.framework.result.BusinessResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author xujie
+ * @description iCreditBanner类
+ * @create 2021-08-19 14:17
+ **/
+@Component
+@FeignClient("workspace")
+public interface UserWorkspaceFeignClient {
+
+    @GetMapping("/workspace/user/getWorkspaceByUserId/{id}")
+    BusinessResult<List<Map<String, String>>> getWorkspaceListByUserId(@PathVariable(value = "id", required = false) String id);
+}
